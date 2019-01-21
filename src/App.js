43839cde793d2f55bb2d0c675470ecdf295a6c8b
chrissi <christina.rost@hfg-gmuend.de>
2019-01-21 18:20:28 +0100

    mapped finger positions to screen size

--- src/App.js
+++ src/App.js
@@ -17,15 +17,18 @@ class App extends Component {
       active: null,
       rightThumb: {
         x: -10,
-        y: -10
+        y: -10,
+        z: -10
       },
       rightIndex: {
         x: -10,
-        y: -10
+        y: -10,
+        z: -10
       },
       leftThumb: {
         x: -10,
-        y: -10
+        y: -10,
+        z: -10
       },
       leftIndex: {
         x: -10,
@@ -114,22 +117,15 @@ class App extends Component {
     this.leftIndex = [-10, -10];
     this.rightThumb = [-10, -10];
     this.rightIndex = [-10, -10];
-    // this.fingers = 0;
     if (this.hands && this.hands.length){
       this.hands.forEach((hand, index) => {
         if (hand.type === "left") {
           this.leftIndex = this.getFingerScreenPosition(hand.thumb.dipPosition);
-          this.leftThumb = this.getFingerScreenPosition(
-            hand.indexFinger.dipPosition
-          );
-          this.fingers[index] = [this.leftIndex, this.leftThumb];
+          this.leftThumb =  this.getFingerScreenPosition(hand.indexFinger.dipPosition );
           console.log("left hand: " + this.leftIndex + ", " + this.leftThumb);
         } else {
           this.rightIndex = this.getFingerScreenPosition(hand.thumb.dipPosition);
-          this.rightThumb = this.getFingerScreenPosition(
-            hand.indexFinger.dipPosition
-          );
-          // this.fingers[index] = [this.rightIndex, this.rightThumb];
+          this.rightThumb = this.getFingerScreenPosition(hand.indexFinger.dipPosition);
           console.log("right hand: " + this.rightIndex + ", " + this.rightThumb);
         }
       })
@@ -142,36 +138,30 @@ class App extends Component {
       x: this.leftIndex[0],
       y: this.leftIndex[1]
     }})
-    this.setState({rightIndex: {x: this.rightIndex[0], y: this.rightIndex[1]}})
-    this.setState({leftThumb: {x: this.leftThumb[0], y: this.leftThumb[1]}})
-    this.setState({rightThumb: {x: this.rightThumb[0], y: this.rightThumb[1]}})
+    this.setState({rightIndex: {x: this.rightIndex[0], y: this.rightIndex[1], z: this.rightIndex[2]}})
+    this.setState({leftIndex:  {x: this.leftIndex[0],  y: this.leftIndex[1],  z: this.leftIndex[2] }})
+    this.setState({leftThumb:  {x: this.leftThumb[0],  y: this.leftThumb[1],  z: this.leftThumb[2] }})
+    this.setState({rightThumb: {x: this.rightThumb[0], y: this.rightThumb[1], z: this.rightThumb[1]}})
   }
 
   // functions to set the circle position according to finger tip position detected by leap
-  restrictPosToRange(value, min, max) {
-    if (value > max) {
-      return max;
-    }
-    if (value < min) {
-      return min;
-    }
-    return value;
+  mapVal(val, in_min, in_max, out_min, out_max) {
+    return (val - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
   }
-  getFingerScreenPosition(leapPosition, spaceSize) {
-    const heightStartScale = 100;
-    const widthRatio = window.innerWidth / (2 * spaceSize);
-    const heightRatio = window.innerHeight / (2 * spaceSize);
-    const x = this.restrictPosToRange(leapPosition[0], -spaceSize, spaceSize);
-    const y = this.restrictPosToRange(
-      leapPosition[1] - heightStartScale - spaceSize,
-      -spaceSize,
-      spaceSize
-    );
-    const z = leapPosition[2];
-    const xNorm = x * widthRatio;
-    const yNorm = y * heightRatio;
-    const normalizedPosition = [xNorm, yNorm, z];
-    return normalizedPosition;
+  getFingerScreenPosition(leapPosition, spaceSize=50) {
+    const INTERACTION_SPACE_WIDTH = 200
+    const INTERACTION_SPACE_DEPTH = 120
+
+    const max_width = 2* INTERACTION_SPACE_WIDTH
+    const max_depth = 2* INTERACTION_SPACE_DEPTH
+
+    const x = this.mapVal(leapPosition[0], -INTERACTION_SPACE_WIDTH, INTERACTION_SPACE_WIDTH, 0, window.innerWidth)
+    const y = this.mapVal(leapPosition[1], -INTERACTION_SPACE_DEPTH, INTERACTION_SPACE_DEPTH, 0, window.innerHeight)
+    const z = this.mapVal(leapPosition[2], -INTERACTION_SPACE_WIDTH, INTERACTION_SPACE_WIDTH, 0, window.innerWidth)
+
+    const pos = [x, y, z]
+    return pos
+
   }
 
 
@@ -198,34 +188,6 @@ class App extends Component {
 
 
   render() {
-    // frame = this.state.frame
-    // this.hands = frame.hands
-    // this.leftThumb = [-10, -10];
-    // this.leftIndex = [-10, -10];
-    // this.rightThumb = [-10, -10];
-    // this.rightIndex = [-10, -10];
-    // this.fingers = 0;
-    // if (this.hands && this.hands.length){
-    //   this.hands.forEach((hand, index) => {
-    //     if (hand.type === "left") {
-    //       this.leftIndex = this.getFingerScreenPosition(hand.thumb.dipPosition);
-    //       this.leftThumb = this.getFingerScreenPosition(
-    //         hand.indexFinger.dipPosition
-    //       );
-    //       this.fingers[index] = [this.leftIndex, this.leftThumb];
-    //       console.log("left hand: " + this.leftIndex + ", " + this.leftThumb);
-    //     } else {
-    //       this.rightIndex = this.getFingerScreenPosition(hand.thumb.dipPosition);
-    //       this.rightThumb = this.getFingerScreenPosition(
-    //         hand.indexFinger.dipPosition
-    //       );
-    //       this.fingers[index] = [this.rightIndex, this.rightThumb];
-    //       console.log("right hand: " + this.rightIndex + ", " + this.rightThumb);
-    //     }
-    //   })
-    // } else{
-    //   console.log("no hands")
-    // }
 
     let button;
     if (this.state.active) {
@@ -307,8 +269,8 @@ class App extends Component {
           </Router>
           <div className="right_index" left={this.state.rightIndex.x} top={this.state.rightIndex.y}/>
           <div className="right_thumb" left={this.state.rightThumb.x} top={this.state.rightThumb.y}/>
-          <div className="left_index" left={this.state.leftIndex.x} top={this.state.leftIndex.y}/>
-          <div className="left_thumb" left={this.state.leftThumb.x} top={this.state.leftThumb.y}/>
+          <div className="left_index"  left={this.state.leftIndex.x}  top={this.state.leftIndex.y}/>
+          <div className="left_thumb"  left={this.state.leftThumb.x}  top={this.state.leftThumb.y}/>
         </MyProvider>
       </div>
     ); // end of return
