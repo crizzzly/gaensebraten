dd1a3659cb9ba23b27f3d565e3774c5d7dcd8146
flughafen <spiel.hoelle@live.de>
2019-01-25 21:38:35 +0100

    conditional bg-class login view

--- src/Login.js
+++ src/Login.js
@@ -4,21 +4,33 @@ import { Link } from "react-router-dom";
 class Login extends Component {
   render() {
     return (
-      <div className="Login">
-        <h1 className="">Handfree cookbook</h1>
-        <h2 className="">Please sign in</h2>
-        <div>
-          <input placeholder="username" className="form-control" type="text" name="username" />
+      <div className="Login bg-primary">
+        <div className="">
+          <h1 className="text-white">Handfree cookbook</h1>
+          <h2 className="text-white">Please sign in</h2>
+          <div className="my-4">
+            <input
+              placeholder="username"
+              className="form-control"
+              type="text"
+              name="username"
+            />
+            <input
+              className="form-control"
+              type="password"
+              name="p assword"
+              placeholder="password"
+            />
+          </div>
+          <div className="checkbox mb-3">
+            <label className="text-white">
+              <input type="checkbox" value="remember-me" /> Remember me
+            </label>
+          </div>
+          <Link className="btn btn-lg btn-outline-light btn-block" to="/receipts/">
+            Login
+          </Link>
         </div>
-        <div>
-          <input className="form-control" type="password" name="p assword" placeholder="password" />
-        </div>
-        <div className="checkbox mb-3">
-          <label>
-            <input type="checkbox" value="remember-me"/> Remember me
-          </label>
-        </div>
-        <Link className="btn btn-lg btn-primary btn-block" to="/receipts/">Login</Link>
       </div>
     );
   }
